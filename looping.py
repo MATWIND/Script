c4243a4784fb228d6a0de2c03b60b3561bb412fd
MATWIND <65644369+MATWIND@users.noreply.github.com>
2020-06-05 12:59:09 +0700

    Add files via upload

---
+++ looping.py
@@ -0,0 +1,15 @@
+import os
+
+os.system('clear')
+print "======================================="
+print "= Created: MATWIND-OFFICIAL           ="
+print "= Youtube: Telor Studio               ="
+print "= Kind   : LOOPING                    ="
+print "= Github : https://github.com/MATWIND ="
+print "======================================="
+print " "
+x = input ("Klik enter untuk lanjut")
+os.system('clear')
+
+while(True):
+    print "ANDA TERKENA VIRUS" 
