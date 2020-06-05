dc173f895cac959d0410ec9db1e275ef53bb41c7
MATWIND <65644369+MATWIND@users.noreply.github.com>
2020-06-05 13:16:53 +0700

    Update looping.py

--- looping.py
+++ looping.py
@@ -1,15 +1,2 @@
-import os
-
-os.system('clear')
-print "======================================="
-print "= Created: MATWIND-OFFICIAL           ="
-print "= Youtube: Telor Studio               ="
-print "= Kind   : LOOPING                    ="
-print "= Github : https://github.com/MATWIND ="
-print "======================================="
-print " "
-x = str(input("Ketik y untuk lanjut :  "))
-os.system('clear')
-
-while x : "y"
+while(True) :
     print "ANDA TERKENA VIRUS" 
